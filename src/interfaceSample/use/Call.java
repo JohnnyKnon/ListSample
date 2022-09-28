050e46925f20817c149828c61109713231c24ea4
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-09-28 14:48:30 +0900

    주석 추가

--- src/interfaceSample/use/Call.java
+++ src/interfaceSample/use/Call.java
@@ -3,7 +3,9 @@ package interfaceSample.use;
 import interfaceSample.used.AddCalc;
 import interfaceSample.used.SubCalc;
 import interfaceSample.used.Calculator;
-
+/**
+ * 인터페이스 의존 확인
+ * */
 public class Call {
     public static void main(String[] args){
         Calculator calculator = new SubCalc();
